9da25c20306489581fd1694b2dffde951bf7b97a
Wei Zhang <iusenenow@gmail.com>
2020-08-19 10:27:28 +1000

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAtxQ2gGRmn7wOygNiM_hEskSse9ZOCD7c",
+  authDomain: "clone-58980.firebaseapp.com",
+  databaseURL: "https://clone-58980.firebaseio.com",
+  projectId: "clone-58980",
+  storageBucket: "clone-58980.appspot.com",
+  messagingSenderId: "820549947075",
+  appId: "1:820549947075:web:a346fffe4170d153399c76"
+};

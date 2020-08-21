69167eb7e38fd8ed49374b839a8d837b9bd59c2c
Wei Zhang <iusenenow@gmail.com>
2020-08-21 19:32:45 +1000

    modify

--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,57 @@ const Home = () => {
     <div className="home">
       <img src="https://images-fe.ssl-images-amazon.com/images/G/35/digital/video/merch/2020/TV/ECCH_S1_00000_GWBleedingHero_3000x1200_POST_Final_en-AU_PVD5268._CB408557760_.jpg" alt="" className="home-image" />
 
-      <Product
-        id="12321341"
-        title='LG 38WN95C 38" UWQHD+ Curved Nano IPS Monitor'
-        price={'2,299.00'}
-        rating={5}
-        image="https://images-fe.ssl-images-amazon.com/images/I/71iRe--kVtL._AC_SY400_.jpg"
-      />
+      <div className="home-row">
+        <Product
+          id="1"
+          title='Nintendo Switch Console [Neon Blue/Red]'
+          price={'448.00'}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51vKlrdRKYL._AC_UL480_SR480,480_.jpg"
+        />
+        <Product
+          id="2"
+          title='Ghost of Tsushima - PlayStation 4'
+          price={'69.00'}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/71kpwRGYwqL._AC_UL480_SR381,480_.jpg"
+        />
+      </div>
+
+      <div className="home-row">
+        <Product
+          id="3"
+          title='The Last of Us Part 2 - PlayStation 4'
+          price={'68.00'}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/71T0ogpJS1L._AC_UL405_SR321,405_.jpg"
+        />
+        <Product
+          id="4"
+          title='Kindle Paperwhite – Now Waterproof with twice the Storage (8GB)'
+          price={'199.00'}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/61eAq6gg-XL._AC_UL480_SR480,480_.jpg"
+        />
+        <Product
+          id="5"
+          title='Echo Dot (3rd Gen) – Smart speaker with Alexa - Charcoal Fabric'
+          price={'69.00'}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/61MdyHSbxHL._AC_UL480_SR480,480_.jpg"
+        />
+      </div>
+
+      <div className="home-row">
+        <Product
+          id="5"
+          title='LG 38WN95C 38" UWQHD+ Curved Nano IPS Monitor'
+          price={'2,299.00'}
+          rating={6}
+          image="https://images-fe.ssl-images-amazon.com/images/I/71iRe--kVtL._AC_SY400_.jpg"
+        />
+      </div>
+
     </div>
   )
 }
--- src/Product.js
+++ src/Product.js
@@ -3,20 +3,22 @@ import React from 'react'
 const Product = ({ id, title, image, price, rating }) => {
   return (
     <div className="product">
-      <h3>{title}</h3>
-      <p className="product-price">
-        <small>$</small>
-        <strong>{price}</strong>
-      </p>
-      <div className="product-rating">
-        {
-          Array(rating)
-            .fill()
-            .map((_) => (
+      <div className="product-info">
+        <h4>{title}</h4>
+        <p className="product-price">
+          <strong>${price}</strong>
+        </p>
+        <div className="product-rating">
+          {
+            Array(rating).fill().map(_ => (
               <p>⭐</p>
             ))
-        }
+          }
+        </div>
       </div>
+
+      <img src={image} alt="image" />
+      <button>Add to Cart</button>
     </div>
   )
 }

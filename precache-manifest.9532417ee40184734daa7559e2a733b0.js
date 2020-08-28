68ee7ee264eb2edc2c50baeba02594715dfc24db
Wei Zhang <iusenenow@gmail.com>
2020-08-28 12:15:57 +1000

    Updates

---
+++ precache-manifest.9532417ee40184734daa7559e2a733b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b929789d7c0963d0118a1be2634750c6",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "c26aa3770eaf689e701e",
+    "url": "/amazon-clone/static/css/main.e1667338.chunk.css"
+  },
+  {
+    "revision": "f125dff8e12e44a3009a",
+    "url": "/amazon-clone/static/js/2.2aac78f1.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.2aac78f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26aa3770eaf689e701e",
+    "url": "/amazon-clone/static/js/main.99c78976.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
